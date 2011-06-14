6c8f494db202173334c0bb0d4a5c64ea277dd3af
Jack Moffitt <jack@metajack.im>
2011-06-14 08:43:05 -0600

    Fix event and occurence models.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :department
+  has_many :occurences
 
-  validates_presence_of :title, :description, :location, :submitter, :approved, :visible, :featured, :department
+  validates_presence_of :title, :description, :location, :submitter, :department
   validates_associated :department
 end
--- app/models/occurence.rb
+++ app/models/occurence.rb
@@ -1,6 +1,6 @@
 class Occurence < ActiveRecord::Base
   belongs_to :event
 
-  validates_presence_of :date, :time, :duration, :event
+  validates_presence_of :date, :time, :duration
   validates_associated :event
 end
